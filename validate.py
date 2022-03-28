6e479b8840522b203b0525a7dcd0796e20d63782
Peter Zhang <petez@berkeley.edu>
2022-03-28 00:29:34 -0400

    add comment
    add description comnent to validation

--- validate.py
+++ validate.py
@@ -1,3 +1,8 @@
+"""
+Validates scraped data against manually collected
+validation set.
+"""
+
 import pandas as pd
 
 scraped_path = 'data/data.json'
@@ -6,8 +11,7 @@ scraped_df = pd.read_json(scraped_path)
 validation_path = 'data/validation.json'
 validation_df = pd.read_json(validation_path)
 
-expected_legnth = len(scraped_df)
+expected_length = len(scraped_df)
 deduped_df = pd.concat([scraped_df, validation_df]).drop_duplicates(keep=False)
-if len(deduped_df) > scraped_df:
+if len(deduped_df) > expected_length:
     print("Disagreement in samples.")
-    
