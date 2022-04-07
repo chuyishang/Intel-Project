e96bbd84264068af255579d72b2a3c2bd7a2dc2d
Peter Zhang <petez@berkeley.edu>
2022-04-06 23:11:04 -0700

    validation code

--- validate.py
+++ validate.py
@@ -14,11 +14,21 @@ parser.add_argument('--filepath',
                     help='path to the ataset',
                     default='data/scraped_data.json')
 
+d_types = {
+    'company':str,
+    'quarter':str,
+    'year':str,
+    'metric':str,
+    'value':str,
+    'sub-metric':str
+}
+
 # read in scraped file
 scraped_filepath = parser.parse_args().filepath
 scraped_json = json.load(open(scraped_filepath, 'r'))
 scraped_df = pd.json_normalize(scraped_json)
 
+scraped_df = scraped_df.astype(d_types)
 
 # read in validation dataset
 
@@ -26,12 +36,12 @@ sheet_id = "1CtlCDjvO5aZkMaUncdEMQMTAHG2QUjCUqqre4X_FZsk"
 sheet_name = 'validation'
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 validation_df = pd.read_csv(url, error_bad_lines=False, thousands = ",") # Ignore errors
+validation_df = validation_df.astype(d_types)
 
-"""url = "data/validate.csv"
-validation_df = pd.read_csv(url, error_bad_lines=True) # Ignore errors"""
+
 
 # check expected length
-deduped_df = pd.concat([scraped_df, validation_df]).drop_duplicates(keep=False)
+deduped_df = pd.concat([scraped_df, validation_df]).drop_duplicates(keep="first")
 expected_length = len(scraped_df)
 actual_length = len(deduped_df)
 
@@ -41,10 +51,13 @@ if actual_length == expected_length:
     quit()
 
 # if error
-print("Disagreement in samples.")
+print(f"Disagreement in {actual_length - expected_length} samples.")
 
-merged_df = validation_df.merge(scraped_df, indicator='left_only')
-merged_df
+
+merged_df = validation_df.merge(scraped_df, how = 'outer', on = ['company', 'quarter', 'year', 'metric', 'sub-metric'], indicator='source')
+error_df = merged_df[merged_df['source'] == 'left_only']
+print(error_df)
+
 """for i in len(merged_df):
     if merged_df["_merge"][i] == "left_only":
         print merged_df"""
