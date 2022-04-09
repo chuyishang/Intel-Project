1304b5cc9a88a3984d9269b210e4481f1425be58
mirandalshen <miranda.l.shen@berkeley.edu>
2022-04-09 11:10:18 -0700

    update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from prophet import Prophet
 import json
 
@@ -25,12 +26,27 @@ fig2 = m.plot_components(forecast)
 """
 Processes data for TSMC Capex, turns json into dataframe
 """
-df = pd.read_json('data/tsmc_json_data.json')
-df["date"] = df["year"].astype(str) + df["quarter"]
-df = df.drop(["year", "quarter", "company", "sub-metric"], axis=1)
-df = df[['metric', 'date', 'value']]
-df = df.loc[df['metric'] == 'capex']
-df = df.drop(["metric"], axis=1)
+
+with open('data/tsmc_json_data.json') as f:
+   data = json.load(f)
+tsmc_df = pd.DataFrame(data)
+# df = pd.read_json('data/tsmc_json_data.json', lines=False)
+for i in np.arange(len(tsmc_df)):
+    if int(tsmc_df["year"][i]) >= 0:
+        tsmc_df["year"][i] = "20" + tsmc_df["year"][i]
+    else:
+        tsmc_df["year"][i] = "19" + tsmc_df["year"][i]
+for i in np.arange(len(tsmc_df)):
+    tsmc_df["quarter"][i] = tsmc_df["quarter"][i][::-1]
+tsmc_df["combined"] = tsmc_df["year"].astype(str) + "-" + tsmc_df["quarter"].astype(str)
+tsmc_df["ds"] = pd.to_datetime(tsmc_df['combined'], errors='coerce')
+tsmc_df = tsmc_df.drop(["combined", "year", "quarter", "company", "sub-metric"], axis=1)
+tsmc_df = tsmc_df[['metric', 'ds', 'value']]
+tsmc_df = tsmc_df.loc[tsmc_df['metric'] == 'capex']
+tsmc_df = tsmc_df.drop(["metric"], axis=1)
+tsmc_df = tsmc_df.rename(columns={"value": "y"})
+# print(tsmc_df)
+
 """
 Returns df with one column with year + quarter and one column as value
 """
@@ -40,17 +56,20 @@ Returns df with one column with year + quarter and one column as value
 Forecasts data given df and timeframe
 """
 def fut_forecast(df, timeframe):
-    
+
     m = Prophet()
     m.fit(df)
     # need to convert year and quarter to single datetime field, name column "date"
 
 
-    future = m.make_future_dataframe(freq="year",periods=len(timeframe))
+    future = m.make_future_dataframe(periods=len(timeframe))
     future.tail()
 
     forecast = m.predict(future)
-    forecast[['date', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+    # forecast[['date', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 
-    fig1 = m.plot(forecast)
-    fig2 = m.plot_components(forecast)
+    # fig1 = m.plot(forecast)
+    # fig2 = m.plot_components(forecast)
+
+print(fut_forecast(tsmc_df, [2003,2004,2005]))
