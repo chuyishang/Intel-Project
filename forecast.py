2cd22e9c24dd3f8e292706b5828e342b626ffc15
beneric123 <95617190+beneric123@users.noreply.github.com>
2022-04-09 16:05:51 -0700

    refactored forecast.py code

--- forecast.py
+++ forecast.py
@@ -4,53 +4,37 @@ from prophet import Prophet
 import json
 
 """
-
-df = pd.read_csv('../examples/example_wp_log_peyton_manning.csv')
-df.head()
-
-m = Prophet()
-m.fit(df)
-
-future = m.make_future_dataframe(periods=365)
-future.tail()
-
-forecast = m.predict(future)
-forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-
-fig1 = m.plot(forecast)
-
-fig2 = m.plot_components(forecast)
+Converts json for specific company to df
+"""
+def json_to_df(json_path):
+    with open(json_path) as f:
+        data = json.load(f)
+    df = pd.DataFrame(data)
+    return df
 
 """
-
+Reformats df to have two columns, one datetime column and one value column
 """
-Processes data for TSMC Capex, turns json into dataframe
-"""
+def df_datetime_metric(df, metric):
+    for i in np.arange(len(df)):
+        if int(df["year"][i]) >= 0 and int(df["year"][i]) <= 21:
+            df["year"][i] = "20" + df["year"][i]
+        else:
+            df["year"][i] = "19" + df["year"][i]
+    for i in np.arange(len(df)):
+        df["quarter"][i] = df["quarter"][i][::-1]
+    df["combined"] = df["year"].astype(str) + "-" + df["quarter"].astype(str)
+    df["ds"] = pd.to_datetime(df['combined'], errors='coerce')
+    df = df.drop(["combined", "year", "quarter", "company", "sub-metric"], axis=1)
+    df = df[['metric', 'ds', 'value']]
+    df = df.loc[df['metric'] == metric]
+    df = df.drop(["metric"], axis=1)
+    df = df.rename(columns={"value": "y"})
+    return df
 
-with open('data/tsmc_json_data.json') as f:
-   data = json.load(f)
-tsmc_df = pd.DataFrame(data)
-# df = pd.read_json('data/tsmc_json_data.json', lines=False)
-for i in np.arange(len(tsmc_df)):
-    if int(tsmc_df["year"][i]) >= 0 and int(tsmc_df["year"][i]) <= 21:
-        tsmc_df["year"][i] = "20" + tsmc_df["year"][i]
-    else:
-        tsmc_df["year"][i] = "19" + tsmc_df["year"][i]
-for i in np.arange(len(tsmc_df)):
-    tsmc_df["quarter"][i] = tsmc_df["quarter"][i][::-1]
-tsmc_df["combined"] = tsmc_df["year"].astype(str) + "-" + tsmc_df["quarter"].astype(str)
-tsmc_df["ds"] = pd.to_datetime(tsmc_df['combined'], errors='coerce')
-tsmc_df = tsmc_df.drop(["combined", "year", "quarter", "company", "sub-metric"], axis=1)
-tsmc_df = tsmc_df[['metric', 'ds', 'value']]
-tsmc_df = tsmc_df.loc[tsmc_df['metric'] == 'capex']
-tsmc_df = tsmc_df.drop(["metric"], axis=1)
-tsmc_df = tsmc_df.rename(columns={"value": "y"})
-# print(tsmc_df)
-
-"""
-Returns df with one column with year + quarter and one column as value
-"""
-
+tsmc_df = json_to_df('data/tsmc_json_data.json')
+tsmc_capex_df = df_datetime_metric(tsmc_df, 'capex')
+print(tsmc_capex_df)
 
 """
 Forecasts data given df and timeframe
@@ -59,9 +43,7 @@ def fut_forecast(df, timeframe):
 
     m = Prophet()
     m.fit(df)
-    # need to convert year and quarter to single datetime field, name column "date"
-
-
+    
     future = m.make_future_dataframe(periods=len(timeframe))
     future.tail()
 
@@ -72,4 +54,7 @@ def fut_forecast(df, timeframe):
     # fig1 = m.plot(forecast)
     # fig2 = m.plot_components(forecast)
 
-print(fut_forecast(tsmc_df, [2003,2004,2005]))
+print(fut_forecast(tsmc_capex_df, [2003,2004,2005]))
+
+# TO DO: Process data for metrics with submetrics
+# QUESTION: Plot multiple companies forecasts together?
