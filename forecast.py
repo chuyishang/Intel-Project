1ba6413e4fa22e0d5f4077fde1f0e637cea23789
mirandalshen <miranda.l.shen@berkeley.edu>
2022-04-17 19:01:50 -0700

    finished forecast.py and forecast visualization and

--- forecast.py
+++ forecast.py
@@ -6,11 +6,9 @@ import json
 """
 Reformats df to have two columns, one datetime column and one value column
 """
-def df_datetime_metric(json_f, metric):
-    with open("data/" + json_f) as f:
-        data = json.load(f)
-    df = pd.DataFrame(data)
-    df["combined"] = df["year"].astype(str) + "-" + df["quarter"].astype(str)
+
+def df_datetime_metric(df, metric):
+    df["combined"] = df["year"].astype(str) + "-" + "Q" + df["quarter"].astype(str)
     df["ds"] = pd.to_datetime(df['combined'], errors='coerce')
     df = df.drop(["combined", "year", "quarter", "company", "sub-metric"], axis=1)
     df = df[['metric', 'ds', 'value']]
@@ -27,13 +25,11 @@ def fut_forecast(df, metric, years=3):
     m = Prophet()
     m.fit(df)
 
-    future = m.make_future_dataframe(periods=years)
+    future = m.make_future_dataframe(periods=years, freq="YS")
 
     forecast = m.predict(future)
-    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    return m, forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]\
 
 # TO DO: Process data for metrics with submetrics
 # QUESTION: Plot multiple companies forecasts together?
 
-# df_datetime_metric("tsmc_json_data.json", 'capex')
-fut_forecast("tsmc_json_data.json", 'capex')
